7b84ee2917fa9eaf7b9a4cabf3adaba52599d400
Juan Murua Olalde <juan@susosise.es>
2019-11-18 15:34:36 +0100

    Primer commit. Una estructura vacia de proyectos recien creados.
    Solo estan
    => el proyecto de EjemploPlantillaWPF,
    => el correspondiente proyecto de tests que le acompaina
    => y un proyecto auxiliar para experimentos/pruebas temporales.

---
+++ EjemploPlantillaWPF_TESTs/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EjemploPlantillaWPF_TESTs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EjemploPlantillaWPF_TESTs")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("84475021-3219-4686-8561-c86d42f856f4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
