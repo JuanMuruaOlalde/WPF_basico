9c17cd805020564e5dab91754c3271fe33959218
Juan Murua Olalde <juan@susosise.es>
2019-11-18 17:04:28 +0100

    Incorporar un modelo de vista para controlar lo presentado en la ventana. Poner algunos textos en la ventana.
    Se muestra la interaccion basica entre una ventana de presentacion (MainWindow.xaml)
    y el correspondiente modelo de vista (MainViewModel.cs) que soporta su contenido.
    
    Por ahora, solo se han presentado algunos textos. Utilizando unos marcos simples para situarlos.

---
+++ EjemploPlantillaWPF/MainViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploPlantillaWPF
+{
+    public class MainViewModel : System.ComponentModel.INotifyPropertyChanged
+    {
+        private string _mensajeEnLaBarraDeEstado;
+        public string mensajeEnLaBarraDeEstado
+        {
+            get { return _mensajeEnLaBarraDeEstado; }
+            set
+            {
+                _mensajeEnLaBarraDeEstado = value;
+                NotificarQueAlgoHaCambiadoEn("mensajeEnLaBarraDeEstado");
+            }
+        }
+
+        private string _textoDePrueba1;
+        public string textoDePrueba1
+        {
+            get { return _textoDePrueba1; }
+            set
+            {
+                _textoDePrueba1 = value;
+                NotificarQueAlgoHaCambiadoEn("textoDePrueba1");
+
+                mensajeEnLaBarraDeEstado = "Has tecleado '" + textoDePrueba1 + "' en la prueba 1.";
+            }
+        }
+
+        private string _textoDePrueba2;
+        public string textoDePrueba2
+        {
+            get { return _textoDePrueba2; }
+            set
+            {
+                _textoDePrueba2 = value;
+                NotificarQueAlgoHaCambiadoEn("textoDePrueba2");
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotificarQueAlgoHaCambiadoEn(string nombreDeLaPropiedad)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(sender: this, e: new System.ComponentModel.PropertyChangedEventArgs(nombreDeLaPropiedad));
+            }
+        }
+
+    }
+
+
+}
---
+++ EjemploPlantillaWPF_TESTs/PruebasDelInterfaceDeUsuario.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Collections.Generic;
+
+
+namespace EjemploPlantillaWPF_TESTs
+{
+    [TestClass]
+    public class PruebasDelInterfaceDeUsuario
+    {
+        [TestMethod]
+        public void SeNotificanLosCambiosEnLaPropiedad_mensajeEnLaBarraDeEstado()
+        {
+            EjemploPlantillaWPF.MainViewModel modelo = new EjemploPlantillaWPF.MainViewModel();
+
+            List<string> eventos = new List<string>();
+            modelo.PropertyChanged += delegate (object sender, System.ComponentModel.PropertyChangedEventArgs e)
+            {
+                eventos.Add(e.PropertyName);
+            };
+
+            modelo.mensajeEnLaBarraDeEstado = "Hola, mundo.";
+            Assert.AreEqual(1, eventos.Count);
+            Assert.AreEqual("mensajeEnLaBarraDeEstado", eventos[0]);
+        }
+
+        [TestMethod]
+        public void SeNotificanLosCambiosEnLaPropiedad_textoDePrueba1YSeActualizaLaBarraDeEstado()
+        {
+            EjemploPlantillaWPF.MainViewModel modelo = new EjemploPlantillaWPF.MainViewModel();
+
+            List<string> eventos = new List<string>();
+            modelo.PropertyChanged += delegate (object sender, System.ComponentModel.PropertyChangedEventArgs e)
+            {
+                eventos.Add(e.PropertyName);
+            };
+
+            modelo.textoDePrueba1 = "Hola, mundo.";
+            Assert.AreEqual(2, eventos.Count);
+            Assert.AreEqual("textoDePrueba1", eventos[0]);
+            Assert.AreEqual("mensajeEnLaBarraDeEstado", eventos[1]);
+        }
+
+        [TestMethod]
+        public void SeNotificanLosCambiosEnLaPropiedad_textoDePrueba2()
+        {
+            EjemploPlantillaWPF.MainViewModel modelo = new EjemploPlantillaWPF.MainViewModel();
+
+            List<string> eventos = new List<string>();
+            modelo.PropertyChanged += delegate (object sender, System.ComponentModel.PropertyChangedEventArgs e)
+            {
+                eventos.Add(e.PropertyName);
+            };
+
+            modelo.textoDePrueba2 = "Hola, mundo.";
+            Assert.AreEqual(1, eventos.Count);
+            Assert.AreEqual("textoDePrueba2", eventos[0]);
+        }
+
+
+
+    }
+}
